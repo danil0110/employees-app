2ee81ea8e7101f9f2e381e0fd4c163f11535560a
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-10-15 14:58:54 +0300

    Clean up and add an app info component

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+  return (
+    <div className='app-info'>
+      <h1>Учет сотрудников</h1>
+      <h2>Количество сотрудников: 0</h2>
+      <h2>Сотрудники с премией: 0</h2>
+    </div>
+  );
+};
+
+export default AppInfo;
